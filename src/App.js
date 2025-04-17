01cdf9941f94f2fbede6b02a6ee734a6386294da
Thanh <t33h00@gmail.com>
2025-04-17 17:54:52 -0400

    4/17

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 
 import './App.css';
 import CheckIn from './components/CheckIn';
-import List from './components/List';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min";
@@ -15,8 +14,6 @@ function App() {
       <Routes>
       <Route path='/' element={<CheckIn/>}/>
         <Route path='/checkin' element={<CheckIn/>}/>
-        <Route path='/navibar' element={<NaviBar/>}/>
-        <Route path='/list' element={<List/>}/>
       </Routes>
     </BrowserRouter>
   </>
--- src/components/NaviBar.js
+++ src/components/NaviBar.js
@@ -7,10 +7,6 @@ function NaviBar() {
         <Navbar className='top' data-bs-theme="dark">
           <Container >
             <Navbar.Brand>Lotus Spa & Nails</Navbar.Brand>
-            <Nav>
-                <Nav.Link href="/checkin">Check-In</Nav.Link>
-              <Nav.Link href="/list">List</Nav.Link>
-            </Nav>
           </Container>
         </Navbar>
       </>
